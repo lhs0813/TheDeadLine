ab4152c367593663ec57a1356363c21ee3806fe2
lhs0813 <ypd01032@naver.com>
2025-06-08 00:31:42 +0900

    [Fix] Player_Controller_UI Connection

---
+++ Assets/Personal_Folder/LHS/Script/Player_Hp_UI.cs
@@ -0,0 +1,34 @@
+﻿using Akila.FPSFramework;
+using NUnit.Framework.Internal;
+using TMPro;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class Player_Hp_UI : MonoBehaviour
+{
+    
+    TextMeshProUGUI[] HpUis;
+    public Image hpBar;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        HpUis = GetComponentsInChildren<TextMeshProUGUI>();
+        //hpBar = transform.GetChild(4).GetComponent<Image>();
+        Player_Manager.PlayerHpChange += HpUiUpdate;
+    }
+
+    void HpUiUpdate(float hp)
+    {
+        HpUis[1].text = hp.ToString("F0");
+        if (hp > 0)
+        {
+            hpBar.fillAmount = (float)(hp / 100); 
+            Debug.Log(hpBar);
+            Debug.Log(hpBar.fillAmount);
+        }
+        else
+            hpBar.fillAmount = 0;
+    }
+}
---
+++ Assets/Personal_Folder/LHS/Script/Player_Manager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System;
+using Akila.FPSFramework;
+
+public class Player_Manager : MonoBehaviour
+{
+    public static Player_Manager Instance { get; private set; }
+
+    public static Action<float> PlayerHpChange;
+
+    public float playerHp;
+    public float MaxHealth;
+
+    private void Awake()
+    {
+        PlayerHpChange = null;
+    }
+
+    void Start()
+    {
+
+
+
+        playerHp = 100;
+        MaxHealth = playerHp;
+
+        PlayerHpChange.Invoke(100);
+
+        PlayerHpChange += hpUpdate;
+
+        
+    }
+
+    private void hpUpdate(float hp)
+    {
+        playerHp = hp;
+    }
+
+}
+
