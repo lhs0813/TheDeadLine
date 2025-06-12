8656fba8ebdac0b6ad6501597064b6b42292dd79
Vomeee <158576443+Vomeee@users.noreply.github.com>
2025-06-12 10:07:57 +0900

    [Feat] 대형 맵기반 맵 생성 기반 닦기

--- Assets/Maps/Scripts/Spawners/Horde/HordeSpawner.cs
+++ Assets/Maps/Scripts/Spawners/Horde/HordeSpawner.cs
@@ -1,49 +1,24 @@
 using UnityEngine;
 using UnityEngine.AI;
-using System.Collections.Generic;
 
 public class HordeSpawner : MonoBehaviour
 {
-    [SerializeField] private float spawnRadius = 5f;
-    [SerializeField] private float minSpacing = 1.5f;
-    [SerializeField] private LayerMask enemyLayer;
-    //[SerializeField] private EnemyType spawnType;
-
+    // 더 이상 spawnRadius, minSpacing, enemyLayer 등은 필요 없겠죠.
     public void TrySpawn(int mapIndex)
     {
         Debug.Log("스폰 시도");
-        int spawnCount = MapGenCalculator.GetCreatureSpawnCountRangePerRoom(mapIndex).GetRandom(new DunGen.RandomStream());
 
-        List<Vector3> validPositions = new List<Vector3>();
-        int attempts = 0;
-        int maxAttempts = spawnCount * 10;
+        int spawnCount = MapGenCalculator
+            .GetCreatureSpawnCountRangePerRoom(mapIndex)
+            .GetRandom(new DunGen.RandomStream());
 
-        while (validPositions.Count < spawnCount && attempts < maxAttempts)
-        {
-            attempts++;
-
-            Vector2 randomCircle = Random.insideUnitCircle * spawnRadius;
-            Vector3 randomPoint = transform.position + new Vector3(randomCircle.x, 0, randomCircle.y);
-
-            if (NavMesh.SamplePosition(randomPoint, out NavMeshHit hit, 2f, NavMesh.AllAreas))
-            {
-                bool tooClose = validPositions.Exists(pos => Vector3.Distance(pos, hit.position) < minSpacing);
-                if (!tooClose)
-                    validPositions.Add(hit.position);
-            }
-        }
-
-        foreach (var pos in validPositions)
+        for (int i = 0; i < spawnCount; i++)
         {
             EnemyType type = HordeSpawnBuilder.RollEnemyType(mapIndex);
-            GameObject enemy = EnemyPoolManager.Instance.Spawn(type, pos, Quaternion.identity);
-            // TODO : enemy?.GetComponent<EnemyClass>()?.Initialize();
+
+            GameObject enemy = EnemyPoolManager
+                .Instance
+                .Spawn(type, transform);
         }
-    }
-
-
-    private int GetSpawnCount(int mapIndex)
-    {
-        return MapGenCalculator.GetCreatureSpawnCountRangePerRoom(mapIndex).GetRandom(new DunGen.RandomStream());
     }
 }
