387600ee7a148a1eb1a4037b96928c8053993f53
lhs0813 <ypd01032@naver.com>
2025-06-16 23:01:31 +0900

    [Fix] 김용찬 주문 해결, MAXHEALTH 관련 Action 및 Damageble 로직 추가
    1. Damagable 에서 PlayerMaxhealth 가 플레이어의 최대 체력!

--- Assets/Personal_Folder/LHS/Script/Player_Hp_UI.cs
+++ Assets/Personal_Folder/LHS/Script/Player_Hp_UI.cs
@@ -10,6 +10,7 @@ public class Player_Hp_UI : MonoBehaviour
     
     TextMeshProUGUI[] HpUis;
     public Image hpBar;
+    private float _maxHp = 100;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,18 +18,26 @@ public class Player_Hp_UI : MonoBehaviour
         HpUis = GetComponentsInChildren<TextMeshProUGUI>();
         //hpBar = transform.GetChild(4).GetComponent<Image>();
         Player_Manager.PlayerHpChange += HpUiUpdate;
+        Player_Manager.PlayerMaxHpChange += MaxHpUiUpdate;
     }
 
     void HpUiUpdate(float hp)
     {
-        HpUis[1].text = hp.ToString("F0");
+        HpUis[1].text = hp.ToString("F0"); // 1번은 NowHealth , 0번은 토탈 헬스~
         if (hp > 0)
         {
-            hpBar.fillAmount = (float)(hp / 100); 
+            hpBar.fillAmount = (float)(hp / _maxHp); 
             Debug.Log(hpBar);
             Debug.Log(hpBar.fillAmount);
         }
         else
             hpBar.fillAmount = 0;
+    }
+
+    void MaxHpUiUpdate(float maxHp)
+    {
+        HpUis[0].text = maxHp.ToString("F0"); // 1번은 NowHealth , 0번은 토탈 헬스~
+        _maxHp = maxHp;
+        
     }
 }
