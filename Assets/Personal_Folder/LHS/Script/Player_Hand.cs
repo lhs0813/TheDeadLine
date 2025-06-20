af275e4bfc0a939f6869226ead0ea4824372e5c3
lhs0813 <ypd01032@naver.com>
2025-06-21 02:54:45 +0900

    [Add] Recharge_Machine
    _Model_Interact

--- Assets/Personal_Folder/LHS/Script/Player_Hand.cs
+++ Assets/Personal_Folder/LHS/Script/Player_Hand.cs
@@ -1,18 +1,30 @@
-﻿using UnityEngine;
+﻿using Akila.FPSFramework;
+using UnityEngine;
 
 public class Player_Hand : MonoBehaviour
 {
     public static Player_Hand Instance { get; private set; }
     public Animator _playerHandAnim;
+    ParticleSystem _spark;
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
     void Start()
     {
         _playerHandAnim = GetComponent<Animator>();
+        
     }
 
     
     public void Charge()
     {
         _playerHandAnim.SetTrigger("Charge");
+
     }
 
 
