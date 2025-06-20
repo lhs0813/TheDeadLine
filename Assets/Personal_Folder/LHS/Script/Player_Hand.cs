c2331331ead5805a59cd54709d6cacda8879454f
lhs0813 <ypd01032@naver.com>
2025-06-21 00:34:06 +0900

    [Add] HandMotion

---
+++ Assets/Personal_Folder/LHS/Script/Player_Hand.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Player_Hand : MonoBehaviour
+{
+    public static Player_Hand Instance { get; private set; }
+    public Animator _playerHandAnim;
+    void Start()
+    {
+        _playerHandAnim = GetComponent<Animator>();
+    }
+
+    
+    public void Charge()
+    {
+        _playerHandAnim.SetTrigger("Charge");
+    }
+
+
+
+}
