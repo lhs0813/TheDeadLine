1863af2be57f501b2fa82c45056f12f0f35582e6
lhs0813 <ypd01032@naver.com>
2025-06-21 17:04:54 +0900

    [Add] Recharge_Machine

---
+++ Assets/Personal_Folder/LHS/Script/CameraShake.cs
@@ -0,0 +1,42 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public static CameraShake Instance;
+
+    
+    private CinemachineBasicMultiChannelPerlin _perlin;
+
+    private float _shakeTimer;
+    private float _shakeDuration;
+    private float _startingIntensity;
+
+    private void Awake()
+    {
+        Instance = this;
+        _perlin = GetComponent<CinemachineVirtualCamera>()
+    .GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+    }
+
+    public void ShakeCamera(float intensity, float duration)
+    {
+        _perlin.m_AmplitudeGain = intensity;
+        _shakeDuration = duration;
+        _shakeTimer = duration;
+        _startingIntensity = intensity;
+    }
+
+    private void Update()
+    {
+        if (_shakeTimer > 0)
+        {
+            _shakeTimer -= Time.deltaTime;
+            if (_shakeTimer <= 0f)
+            {
+                _perlin.m_AmplitudeGain = 0f;
+            }
+        }
+    }
+}
