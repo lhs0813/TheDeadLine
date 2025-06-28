d7fa6ae6f06a0d9fc58f2e3fb68c0fe9461a8c4a
Vomeee <158576443+Vomeee@users.noreply.github.com>
2025-06-28 10:25:00 +0900

    [Feat] 언어 대응 다수 추가

--- Assets/Personal_Folder/KHW/Scripts/UI/UI_Objective_Info.cs
+++ Assets/Personal_Folder/KHW/Scripts/UI/UI_Objective_Info.cs
@@ -1,40 +1,67 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.Localization;
-using UnityEngine.Localization.Settings;
+using UnityEngine.Localization.Components;
 
 public class UI_Objective_Info : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI objectiveText; //목표 표출 텍스트.
-    [SerializeField] LocalizedString findFuseLocalizedString;
+    [SerializeField] LocalizeStringEvent objectiveEvent;   // 하나만 연결
+    [SerializeField] TextMeshProUGUI objectiveText;       // Disable용
+    [SerializeField] bool isActive;
 
     void Start()
     {
         ObjectiveManager.instance.OnFindFuseAction += ShowFuseFindingObjective;
-        ObjectiveManager.instance.OnStartReturnToTheTrainObjectiveAction += ReturnToTheTrainObjective;
+        ObjectiveManager.instance.OnStartReturnToTheTrainObjectiveAction += ShowReturnObjective;
         ObjectiveManager.instance.OnDisableAllObjectiveAction += DisableText;
-
+        
         DisableText();
-    }
-
-    public void ShowFuseFindingObjective(int counter)
-    {
-        objectiveText.text = LocalizationSettings.StringDatabase.GetLocalizedString("Objective Table", "Objective_Find Fuse") + $" : {counter} / 3";
-    }
-    public void ReturnToTheTrainObjective()
-    {
-        objectiveText.text = LocalizationSettings.StringDatabase.GetLocalizedString("Objective Table", "Objective_Return To The Train");
-    }
-
-    public void DisableText()
-    {
-        objectiveText.text = "";
     }
 
     void OnDisable()
     {
-        ObjectiveManager.instance.OnFindFuseAction += ShowFuseFindingObjective;
-        ObjectiveManager.instance.OnStartReturnToTheTrainObjectiveAction += ReturnToTheTrainObjective;
-        ObjectiveManager.instance.OnDisableAllObjectiveAction += DisableText;
+        ObjectiveManager.instance.OnFindFuseAction -= ShowFuseFindingObjective;
+        ObjectiveManager.instance.OnStartReturnToTheTrainObjectiveAction -= ShowReturnObjective;
+        ObjectiveManager.instance.OnDisableAllObjectiveAction  -= DisableText;
+    }
+
+
+    void ShowFuseFindingObjective(int count)
+    {
+        if (!isActive)
+        {
+            objectiveText.gameObject.SetActive(true);
+            isActive = true;
+        }
+       
+        // 1) 키와 인자 바꾸기
+        objectiveEvent.StringReference.TableEntryReference = "Objective_Find Fuse";
+        objectiveEvent.StringReference.Arguments = new object[]{ count };
+        // 2) 즉시 갱신
+        objectiveEvent.RefreshString();
+    }
+
+    void ShowReturnObjective()
+    {
+        if (!isActive)
+        {
+            objectiveText.gameObject.SetActive(true);
+            isActive = true;
+        }
+
+        objectiveEvent.StringReference.TableEntryReference = "Objective_Return To The Train";
+        objectiveEvent.StringReference.Arguments = null;  // 인자 없으므로 null
+        objectiveEvent.RefreshString();
+    }
+
+    void DisableText()
+    {
+        if (isActive)
+        {
+            isActive = false;
+            objectiveText.gameObject.SetActive(false);
+        }
+
+
+
     }
 }
