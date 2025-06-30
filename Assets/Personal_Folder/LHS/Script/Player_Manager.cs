f4ff68f2a9fdaf9d635dc4fdf3266fb3c2ccde43
lhs0813 <ypd01032@naver.com>
2025-06-30 17:06:12 +0900

    [Add] Run Bonus skill

--- Assets/Personal_Folder/LHS/Script/Player_Manager.cs
+++ Assets/Personal_Folder/LHS/Script/Player_Manager.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
 using System;
 using Akila.FPSFramework;
+using System.Collections;
 
 public class Player_Manager : MonoBehaviour
 {
     public static Player_Manager Instance { get; private set; }
+
+    public FirstPersonController playerController;
+    public Inventory inventory;
 
     public static Action<float> PlayerHpChange;
     public static Action<float> PlayerMaxHpChange;
@@ -14,6 +18,8 @@ public class Player_Manager : MonoBehaviour
 
     private void Awake()
     {
+        Instance = this;
+
         PlayerHpChange = null;
         PlayerMaxHpChange = null;
     }
@@ -32,6 +38,20 @@ public class Player_Manager : MonoBehaviour
     private void MaxhpUpdate(float maxHp)
     {
         MaxHealth = maxHp;
+    }
+
+    public IEnumerator SpeedReturn()
+    {
+        Vector3 localPos = inventory.transform.localPosition;
+        localPos.z = -0.3f;
+        inventory.transform.localPosition = localPos;
+
+        yield return new WaitForSeconds(1.0f);
+        playerController.walkSpeed = 5;
+        playerController.sprintSpeed = 10;
+
+        localPos.z = -0.5f;
+        inventory.transform.localPosition = localPos;
     }
 
 
