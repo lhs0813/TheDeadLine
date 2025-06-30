b9481636f67d53ed8a86aaa88a68c825428632cf
Vomeee <158576443+Vomeee@users.noreply.github.com>
2025-06-30 22:35:19 +0900

    [Feat, Fix] 폭탄좀비도 스폰 로직에 포함되도록하고, 해상도 조절 개선

--- Assets/Maps/Scripts/Context/MapGenConstants.cs
+++ Assets/Maps/Scripts/Context/MapGenConstants.cs
@@ -10,16 +10,12 @@ public static class MapGenConstants
 
 
     ///Max Enemy Count On a stage
-    public const int MaxNormalCreatureCountLimitOnStage = 100; //스테이지 내 일반 적의 수의 리미트.
-    public const int BaseMaxNormalCreatureCountOnStage = 80; //하나의 스테이지에 존재할 수 있는 일반 적의 최대 수.
-    public const int MaxNormalCreatureCountOffsetByStage = 2; //스테이지마다 증가하는 존재가능한 일반 적의 최대 수.
+    public const int MaxNormalCreatureCountLimitOnStage = 40; //스테이지 내 일반 적의 수의 리미트.
 
-    public const int MaxBigCreatureCountLimitOnStage = 30; //스테이지 내 거대 적의 수의 리미트.
-    public const int BaseMaxBigCreatureCountOnStage = 10; //하나의 스테이지에 존재할 수 있는 거대 적의 최대 수.
-    public const int MaxBigCreatureCountOffsetByStage = 2; //스테이지마다 증가하는 존재가능한 거대 적의 최대 수.
+    public const int MaxBigCreatureCountLimitOnStage = 10; //스테이지 내 거대 적의 수의 리미트.
 
-    public const int MaxBombCreatureCountLimitOnStage = 30; //스테이지 내 자폭형 적의 수의 리미트.
-    public const int BaseMaxBombCreatureCountOnStage = 10; //하나의 스테이지에 존재할 수 있는 자폭형 적의 최대 수.
-    public const int MaxBombCreatureCountOffsetByStage = 2; //스테이지마다 증가하는 존재가능한 자폭형 적의 최대 수.
+    public const int MaxBombCreatureCountLimitOnStage = 5; //스테이지 내 자폭형 적의 수의 리미트.
+
+    public const int MaxFastCreatureCountLimitOnStage = 10;
 
 }
