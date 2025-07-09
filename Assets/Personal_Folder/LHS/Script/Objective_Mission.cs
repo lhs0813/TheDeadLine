55a3763a88397b486b66a8067daf0acbff0a6e7e
lhs0813 <ypd01032@naver.com>
2025-07-09 19:25:38 +0900

    [Fix] HUD 개편

---
+++ Assets/Personal_Folder/LHS/Script/Objective_Mission.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Objective_Mission : MonoBehaviour
+{
+    Animator anim;
+    public TextMeshProUGUI fuseText;
+    private void Start()
+    {
+        GamePlayManager.instance.OnTrainArriveAction += ObjectiveMission;
+        anim = GetComponent<Animator>();
+    }
+
+    void ObjectiveMission()
+    {
+        int fuseCount = ObjectiveManager.instance.fuseCounterObjective;
+        fuseText.SetText(fuseText.text, fuseCount);
+        anim.SetTrigger("On");
+    }
+}
