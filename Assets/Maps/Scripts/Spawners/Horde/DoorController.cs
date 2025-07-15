aa5dc7edcbf5a54b975cd292a1e3fa1d8118223b
Jebicott <yongchan0302@gmail.com>
2025-07-16 03:06:35 +0900

    [update] 문 소리 추가
    ㅇㅇ

--- Assets/Maps/Scripts/Spawners/Horde/DoorController.cs
+++ Assets/Maps/Scripts/Spawners/Horde/DoorController.cs
@@ -1,14 +1,22 @@
-// DoorController.cs
+﻿// DoorController.cs
 using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
     [SerializeField] private Animator animator;
 
+    // 추가할 부분 ↓
+    [SerializeField] private AudioSource audioSource;    // 문 소리를 재생할 AudioSource
+    [SerializeField] private AudioClip openClip;         // 열릴 때 한 번 재생할 클립
+
     public void OpenDoor()
     {
         animator.ResetTrigger("Close");
         animator.SetTrigger("Open");
+
+        // 사운드 재생
+        if (audioSource != null && openClip != null)
+            audioSource.PlayOneShot(openClip);
     }
 
     public void CloseDoor()
