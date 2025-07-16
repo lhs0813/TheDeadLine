c1aad5edc96a52084a1a11087736c447dd2afe77
lhs0813 <ypd01032@naver.com>
2025-07-16 20:52:31 +0900

    [Fix] Achievement

--- Assets/Personal_Folder/LHS/Script/AchieveMent/AchieveMent_Manager.cs
+++ Assets/Personal_Folder/LHS/Script/AchieveMent/AchieveMent_Manager.cs
@@ -21,6 +21,8 @@ public class AchieveMent_Manager : MonoBehaviour
         {
             Debug.LogError("SteamClient가 초기화되지 않았습니다.");
         }
+
+
     }
 
     public void AddZombieKill()
@@ -30,23 +32,33 @@ public class AchieveMent_Manager : MonoBehaviour
             Debug.LogWarning("SteamClient가 유효하지 않거나 로그인되어 있지 않음");
             return;
         }
-
         SteamUserStats.AddStat("STAT_ZOMBIE_KILLS", 1);
-
         SteamUserStats.StoreStats();
     }
 
-    public void FirstSkillChip()
+    public void GetSkillChip()
     {
         if (!SteamClient.IsValid || !SteamClient.IsLoggedOn)
         {
             Debug.LogWarning("SteamClient가 유효하지 않거나 로그인되어 있지 않음");
             return;
         }
-
-        SteamUserStats.IndicateAchievementProgress("ACHIEVEMENT_FIRSTCHIP",1,1);
+        SteamUserStats.AddStat("STAT_DATACHIP_COUNT", 1);
         SteamUserStats.StoreStats();
     }
+
+    public void AddLegendaryCount()
+    {
+        if (!SteamClient.IsValid || !SteamClient.IsLoggedOn)
+        {
+            Debug.LogWarning("SteamClient가 유효하지 않거나 로그인되어 있지 않음");
+            return;
+        }
+        SteamUserStats.AddStat("STAT_LEGENDARY_COUNT", 1);
+        SteamUserStats.StoreStats();
+    }
+
+
 
 
 }
