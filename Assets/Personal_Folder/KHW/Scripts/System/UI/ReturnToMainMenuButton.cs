1bd97f76fd5b594af1bae0a90d55059b6ac8b28a
lhs0813 <ypd01032@naver.com>
2025-07-22 08:01:39 +0900

    [Fix] 김용찬이 쏘아올린 작은 공

--- Assets/Personal_Folder/KHW/Scripts/System/UI/ReturnToMainMenuButton.cs
+++ Assets/Personal_Folder/KHW/Scripts/System/UI/ReturnToMainMenuButton.cs
@@ -7,6 +7,7 @@ public class ReturnToMainMenuButton : MonoBehaviour
     {
         int currentStage = GamePlayManager.instance.currentMapIndex - 2;
         RecordManager.Instance.RecordInfiniteStage(currentStage);
+        Physics.simulationMode = SimulationMode.FixedUpdate;
         SceneManager.LoadScene("Main_Menu");
     }
 
