a544c68cb07515e86f365c8762cc0cde13c60159
lhs0813 <ypd01032@naver.com>
2025-07-22 05:34:14 +0900

    [Add] 손 색상 메테리얼 옷장 제작 완료

---
+++ Assets/Asset_Files/FPS_Controller/FPS Framework/Art/Materials/Character/Arms/ArmChange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmChange : MonoBehaviour
+{
+    public Material targetMaterial;        // 메테리얼 직접 할당
+    public List<Texture> textures;
+    public int textureIndex = 0;
+
+    public void ChangeArm()
+    {
+        if (targetMaterial != null && textures != null && textureIndex < textures.Count)
+        {
+            targetMaterial.SetTexture("_BaseMap", textures[textureIndex]);
+        }
+    }
+}
