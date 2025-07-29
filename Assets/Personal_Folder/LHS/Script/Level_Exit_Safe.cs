07b544f063a607a7fadbc550f503040501b5194f
lhs0813 <ypd01032@naver.com>
2025-07-30 03:39:10 +0900

    [Fix] 맵 탈출 현상 발생시, 지하철로 순간이동 , 좌표는 리커버리 포지션

---
+++ Assets/Personal_Folder/LHS/Script/Level_Exit_Safe.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Level_Exit_Safe : MonoBehaviour
+{
+    public GameObject SafePosition;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player 트리거 감지됨");
+            other.transform.position = SafePosition.transform.position;
+        }
+    }
+}
